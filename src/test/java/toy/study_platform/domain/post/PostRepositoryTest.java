07d88fd71926956323dbcf0a6461ff93f5c278f4
jiyun-Lee-dev <jiyun.dev0@gmail.com>
2022-04-17 16:24:09 -0700

    추가: post 저장을 위한 repository 추가 및 테스트

---
+++ src/test/java/toy/study_platform/domain/post/PostRepositoryTest.java
@@ -0,0 +1,46 @@
+package toy.study_platform.domain.post;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import toy.study_platform.domain.post.entity.Post;
+import toy.study_platform.domain.post.repository.PostRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@TestPropertySource("classpath:application-test.properties")
+public class PostRepositoryTest {
+    @Autowired
+    private PostRepository postRepository;
+
+    @Test
+    public void post_저장() {
+        String title = "test-title-1";
+        String content = "test-content-1";
+        Long writerId = 0L;
+
+        Post post = new Post.Builder()
+                .title(title)
+                .content(content)
+                .writerId(writerId)
+                .build();
+
+        Post savedPost = postRepository.save(post);
+
+        assertEquals(savedPost.getTitle(), title);
+        assertEquals(savedPost.getContent(), content);
+        assertEquals(savedPost.getWriterId(), writerId);
+
+    }
+
+    @AfterEach
+    public void clean() {
+        postRepository.deleteAll();
+    }
+}
