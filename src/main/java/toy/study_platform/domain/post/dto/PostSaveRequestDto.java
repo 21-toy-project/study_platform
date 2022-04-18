95f5b3a9bfbd76b5e8593c09584208cafac2ba1f
jiyun-Lee-dev <jiyun.dev0@gmail.com>
2022-04-17 17:51:24 -0700

    수정: writerId를 전달받을 수 있게끔 dto 수정

--- src/main/java/toy/study_platform/domain/post/dto/PostSaveRequestDto.java
+++ src/main/java/toy/study_platform/domain/post/dto/PostSaveRequestDto.java
@@ -2,9 +2,18 @@ package toy.study_platform.domain.post.dto;
 
 import toy.study_platform.domain.post.entity.Post;
 
+import javax.validation.constraints.NotBlank;
+
 public class PostSaveRequestDto {
+    @NotBlank
     private String title;
+    @NotBlank
     private String content;
+
+    public PostSaveRequestDto(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
 
     public Post toEntity(Long writerId) {
         return new Post.Builder()
