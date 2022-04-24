49a98e12bc2f963521f343f72240478356f8c939
jiyun-Lee-dev <jiyun.dev0@gmail.com>
2022-04-24 13:11:28 +0900

    수정: post 서비스 중 create 기능을 담당하는 클래스명으로 변경

---
+++ src/main/java/toy/study_platform/domain/post/service/PostCreator.java
@@ -0,0 +1,21 @@
+package toy.study_platform.domain.post.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import toy.study_platform.domain.post.dto.PostRequestDto;
+import toy.study_platform.domain.post.entity.Post;
+import toy.study_platform.domain.post.repository.PostRepository;
+
+@Service
+@Transactional
+public class PostCreator {
+    private final PostRepository postRepository;
+
+    public PostCreator(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Post save(PostRequestDto postSaveRequestDto, Long writerId) {
+        return postRepository.save(postSaveRequestDto.toEntity(writerId));
+    }
+}
