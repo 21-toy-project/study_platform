ede35b9c03927648d9a0a7b5c3cf55960d7a6115
JaeHee-K <xcv20123@naver.com>
2022-04-25 21:42:53 +0900

    modify: spring security config

--- src/main/java/toy/study_platform/config/SpringSecurityConfig.java
+++ src/main/java/toy/study_platform/config/SpringSecurityConfig.java
@@ -1,9 +1,15 @@
 package toy.study_platform.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import toy.study_platform.auth.service.CustomUserDetailsService;
 
 @Configuration
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -14,11 +20,25 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/h2-console/**"); // h2 console 허용
     }
 
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    /**
+     * 비밀번호 암호화 관련 설정
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    /**
+     * 권한 인증 받기(로그인)
+     */
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()// 세션을 사용하지 않고 JWT 토큰을 활용하여 진행, csrf토큰검사를 비활성화
-                .authorizeRequests() // 인증절차에 대한 설정을 진행
-                .antMatchers("/", "/error/*", "/login").permitAll(); // 설정된 url은 인증되지 않더라도 누구든 접근 가능
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
     }
 }
