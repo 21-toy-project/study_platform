ac3525e0e6b6224b9014ff6ec5158328289d5e5d
JaeHee-K <xcv20123@naver.com>
2022-05-01 15:15:29 +0900

    modify: name rule에 따라 매개변수 이름 수정

--- src/main/java/toy/study_platform/auth/repository/UserRepository.java
+++ src/main/java/toy/study_platform/auth/repository/UserRepository.java
@@ -6,5 +6,5 @@ import toy.study_platform.auth.entity.UserInfo;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserInfo, Long> {
-    UserInfo findById(String ID);
+    UserInfo findById(String Id);
 }
