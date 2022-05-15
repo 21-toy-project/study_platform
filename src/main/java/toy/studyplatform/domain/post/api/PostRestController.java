f5275f435344ba09b88ac177b1178e25408156af
jiyun-Lee-dev <jiyun.dev0@gmail.com>
2022-05-15 15:38:15 +0900

    feat: comment 저장 컨트롤러 추가

--- src/main/java/toy/studyplatform/domain/post/api/PostRestController.java
+++ src/main/java/toy/studyplatform/domain/post/api/PostRestController.java
@@ -15,13 +15,13 @@ import toy.studyplatform.domain.post.service.PostCreator;
 public class PostRestController {
     private PostCreator postCreator;
 
-    public PostRestController(PostCreator postCreateService) {
-        this.postCreator = postCreateService;
+    public PostRestController(PostCreator postCreator) {
+        this.postCreator = postCreator;
     }
 
     @PostMapping("/api/posts")
     public ResponseEntity<?> savePost(@Validated @RequestBody SavePostRequestDto dto) {
-        // reqeust header의 토큰값에서 사용자 정보 파싱한 뒤 entity에 추가
+        // TODO: 2022-05-15 reqeust header의 토큰값에서 사용자 정보 파싱한 뒤 entity에 추가
         Long writerId = 0L;
         PostResponseDto postResponseDto = postCreator.save(dto, writerId);
         return ResponseEntity.status(HttpStatus.CREATED).body(postResponseDto);
