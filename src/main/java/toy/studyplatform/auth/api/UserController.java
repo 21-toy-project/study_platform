09730c936a9119f31cc32cf24c7c18ecd9cb16df
JaeHee-K <xcv20123@naver.com>
2022-05-16 21:52:55 +0900

    컨트롤러 클래스명 변경 및 수정사항 적용

---
+++ src/main/java/toy/studyplatform/auth/api/UserController.java
@@ -0,0 +1,29 @@
+package toy.studyplatform.auth.api;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import toy.studyplatform.auth.dto.CreateUserRequestDto;
+import toy.studyplatform.auth.dto.UserResponseDto;
+import toy.studyplatform.auth.service.UserService;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/api/user")
+    public ResponseEntity<Object> saveUser(
+            @RequestBody @Valid CreateUserRequestDto createUserRequestDto) {
+        UserResponseDto userResponseDto = userService.save(createUserRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userResponseDto);
+    }
+}
