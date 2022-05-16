83816e225bd064b2aaba24b815fcd423eae0b58e
JaeHee-K <xcv20123@naver.com>
2022-05-16 21:52:03 +0900

    컨트롤러 및 요청 dto 클래스명 변경을 위한 사전 작업

---
+++ src/main/java/toy/studyplatform/auth/dto/CreateUserRequestDto.java
@@ -0,0 +1,26 @@
+package toy.studyplatform.auth.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import toy.studyplatform.auth.entity.UserInfo;
+
+public class UserRequestDto {
+    @JsonProperty @NotBlank private String id;
+    @JsonProperty @NotBlank private String pw;
+    @JsonProperty @NotBlank private String name;
+    @JsonProperty @NotBlank private String auth;
+
+    private UserRequestDto(String id, String pw, String name, String auth) {
+        this.id = id;
+        this.pw = pw;
+        this.name = name;
+        this.auth = auth;
+    }
+
+    public UserInfo toEntity() {
+        UserInfo userInfo = UserInfo.builder().id(id).pw(pw).name(name).auth(auth).build();
+        return userInfo;
+    }
+}
