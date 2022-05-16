3087c14f6cbf58b6500c04da86840f8e8a43a672
JaeHee-K <xcv20123@naver.com>
2022-05-16 21:58:23 +0900

    SpringSecurityConfig spoless 적용

--- src/main/java/toy/studyplatform/config/SpringSecurityConfig.java
+++ src/main/java/toy/studyplatform/config/SpringSecurityConfig.java
@@ -3,11 +3,11 @@ package toy.studyplatform.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
 import toy.studyplatform.auth.service.CustomUserDetailsService;
 
 @Configuration
@@ -25,21 +25,15 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/h2-console/**"); // h2 console 허용
     }
 
-    /**
-     * 비밀번호 암호화 관련 설정
-     */
+    /** 비밀번호 암호화 관련 설정 */
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    /**
-     * 권한 인증 받기(로그인)
-     */
+    /** 권한 인증 받기(로그인) */
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
+        auth.userDetailsService(customUserDetailsService).passwordEncoder(passwordEncoder());
     }
 }
