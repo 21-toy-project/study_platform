2e162af29852cb5839b9fb331c8c1dafe4a72c68
JaeHee-K <xcv20123@naver.com>
2022-05-16 21:57:11 +0900

    UserRepository spoless 적용

--- src/main/java/toy/studyplatform/auth/repository/UserRepository.java
+++ src/main/java/toy/studyplatform/auth/repository/UserRepository.java
@@ -2,6 +2,7 @@ package toy.studyplatform.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import toy.studyplatform.auth.entity.UserInfo;
 
 @Repository
