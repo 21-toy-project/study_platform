ffee45b5da13a99bd16d42791bdbc4a9121756aa
jiyundev0 <jiyun.dev0@gmail.com>
2022-05-22 14:44:10 +0900

    수정: comment 도메인 모델 테스트에 displayName 추가 및 익명댓글 남길 수 있는 경우에 대한 테스트 코드 추가

--- src/test/java/toy/studyplatform/domain/comment/CommentTest.java
+++ src/test/java/toy/studyplatform/domain/comment/CommentTest.java
@@ -1,6 +1,7 @@
 package toy.studyplatform.domain.comment;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,8 +22,9 @@ public class CommentTest {
     }
 
     @Test
+    @DisplayName("comment 글쓴이 익명댓글 남길수없음 성공 테스트")
     public void comment_글쓴이_익명댓글_남길수없음_성공_테스트() {
-        String commentContent = "comment 저장 성공 테스트 내용 2";
+        String commentContent = "comment 저장 성공 테스트 내용";
         Long commentWriterId = 0L;
         boolean isAnonymous = true;
         Comment comment =
@@ -36,6 +38,26 @@ public class CommentTest {
         assertEquals(comment.getContent(), commentContent);
         assertEquals(comment.getPost(), post);
         assertEquals(comment.isAnonymous(), false);
+        assertEquals(comment.getWriterId(), commentWriterId);
+    }
+
+    @Test
+    @DisplayName("comment 글쓴이가 아닌 사용자는 익명댓글 남길수있음 성공 테스트")
+    public void comment_글쓴이가_아닌_사용자는_익명댓글_남길수있음_성공_테스트() {
+        String commentContent = "comment 저장 성공 테스트 내용";
+        Long commentWriterId = 1L;
+        boolean isAnonymous = true;
+        Comment comment =
+                Comment.builder()
+                        .writerId(commentWriterId)
+                        .post(post)
+                        .isAnonymous(isAnonymous)
+                        .content(commentContent)
+                        .build();
+
+        assertEquals(comment.getContent(), commentContent);
+        assertEquals(comment.getPost(), post);
+        assertEquals(comment.isAnonymous(), true);
         assertEquals(comment.getWriterId(), commentWriterId);
     }
 }
