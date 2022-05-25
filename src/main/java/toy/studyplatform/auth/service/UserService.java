40b76b702f7ff2e3ec827ae14cd2fd6f7dea2411
JaeHee-K <xcv20123@naver.com>
2022-05-25 21:46:23 +0900

    response dto 네이밍 변경으로 인한 컨트롤러와 user 서비스 일부 수정

--- src/main/java/toy/studyplatform/auth/service/UserService.java
+++ src/main/java/toy/studyplatform/auth/service/UserService.java
@@ -3,7 +3,7 @@ package toy.studyplatform.auth.service;
 import org.springframework.stereotype.Service;
 
 import toy.studyplatform.auth.dto.CreateUserRequestDto;
-import toy.studyplatform.auth.dto.UserResponseDto;
+import toy.studyplatform.auth.dto.CreateUserResponseDto;
 import toy.studyplatform.auth.entity.UserInfo;
 import toy.studyplatform.auth.repository.UserRepository;
 
@@ -16,9 +16,9 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
-    public UserResponseDto save(CreateUserRequestDto createUserRequestDto) {
+    public CreateUserResponseDto save(CreateUserRequestDto createUserRequestDto) {
         UserInfo userInfo = userRepository.save(createUserRequestDto.toEntity());
-        return UserResponseDto.from(userInfo);
+        return CreateUserResponseDto.from(userInfo);
     }
 
     public UserInfo findById(String id) {
