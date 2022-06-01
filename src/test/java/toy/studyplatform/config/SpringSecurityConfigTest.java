3ed22a7a2194b1b8508f9dda93e3de19033dd045
JaeHee-K <xcv20123@naver.com>
2022-06-01 21:03:33 +0900

    테스트 구분 및 user 세팅 부분 수정

--- src/test/java/toy/studyplatform/config/SpringSecurityConfigTest.java
+++ src/test/java/toy/studyplatform/config/SpringSecurityConfigTest.java
@@ -8,56 +8,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import toy.studyplatform.auth.dto.CreateUserRequestDto;
-import toy.studyplatform.auth.entity.UserInfo;
-import toy.studyplatform.auth.service.UserService;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
 @AutoConfigureMockMvc
 @ContextConfiguration
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class SpringSecurityConfigTest {
-    @Autowired UserService userService;
     @Autowired MockMvc mockMvc;
 
-    String id = "test1";
-    String pw = "123";
-    String name = "jaehee";
-    String role = "ROLE_ADMIN";
-
-    @BeforeEach
-    public void 유저세팅() {
-        CreateUserRequestDto createUserRequestDto = new CreateUserRequestDto(id, pw, name, role);
-        userService.save(createUserRequestDto);
-    }
-
     @Test
-    @Order(1)
-    @DisplayName("회원가입 확인")
-    public void 회원가입_확인() throws Exception {
-        UserInfo userInfo = userService.findById(id);
-        assertThat(userInfo.getId()).isEqualTo(id);
-        assertThat(userInfo.getPw()).isEqualTo(pw);
-        assertThat(userInfo.getName()).isEqualTo(name);
-        assertThat(userInfo.getRole()).isEqualTo(role);
-    }
-
-    @Test
-    @Order(2)
     @DisplayName("USER 권한 로그인 가능성 여부")
     @WithMockUser(username = "test1", roles = "USER")
     public void USER_권한_로그인_가능성여부() throws Exception {
@@ -65,7 +31,6 @@ class SpringSecurityConfigTest {
     }
 
     @Test
-    @Order(3)
     @DisplayName("ADMIN 권한 로그인 가능성 여부")
     @WithMockUser(username = "test2", roles = "ADMIN")
     public void ADMIN_권한_로그인_가능성여부() throws Exception {
@@ -73,7 +38,6 @@ class SpringSecurityConfigTest {
     }
 
     @Test
-    @Order(4)
     @DisplayName("ADMIN api를 USER사용자가 실패하는지")
     @WithMockUser(username = "test3", roles = "USER")
     public void ADMIN_권한_api_USER사용자_실패여부() throws Exception {
@@ -81,7 +45,6 @@ class SpringSecurityConfigTest {
     }
 
     @Test
-    @Order(5)
     @DisplayName("ADMIN api를 ADMIN사용자가 성공하는지")
     @WithMockUser(username = "test3", roles = "ADMIN")
     public void ADMIN_권한_api_ADMIN사용자_성공여부() throws Exception {
