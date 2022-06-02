33f1ded20864c012845ffd5d49c086d59096c950
JaeHee-K <xcv20123@naver.com>
2022-06-02 15:59:44 +0900

    중복 회원가입 체크 api 추가

--- src/main/java/toy/studyplatform/auth/api/UserController.java
+++ src/main/java/toy/studyplatform/auth/api/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,6 +27,11 @@ public class UserController {
             @RequestBody @Valid CreateUserRequestDto createUserRequestDto) {
         CreateUserResponseDto createUserResponseDto = userService.save(createUserRequestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(createUserResponseDto);
+    }
+
+    @GetMapping("/api/userid/{id}/exists")
+    public ResponseEntity<Boolean> checkIdDuplicate(@PathVariable String id) {
+        return ResponseEntity.ok(userService.checkIdDuplicate(id));
     }
 
     @GetMapping("/admin")
