f5ecc2d3454492a820dd59423aa406a86b690322
jiyundev0 <jiyun.dev0@gmail.com>
2022-06-06 10:51:55 +0900

    test: post 전체 목록 조회 테스트 코드 추가

--- src/test/java/toy/studyplatform/domain/post/PostRepositoryTest.java
+++ src/test/java/toy/studyplatform/domain/post/PostRepositoryTest.java
@@ -2,9 +2,12 @@ package toy.studyplatform.domain.post;
 
 import java.util.List;
 
+import com.querydsl.core.Tuple;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -18,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import toy.studyplatform.config.TestConfig;
 import toy.studyplatform.domain.comment.entity.Comment;
 import toy.studyplatform.domain.comment.repository.jpa.CommentRepository;
-import toy.studyplatform.domain.post.dto.response.FindPostResponseSimpleDto;
 import toy.studyplatform.domain.post.entity.Post;
 import toy.studyplatform.domain.post.repository.jpa.PostRepository;
 
@@ -63,7 +65,7 @@ public class PostRepositoryTest {
         postRepository.save(post2);
 
         // given - comments
-        String commentContent = "comment 저장 성공 테스트 내용";
+        String commentContent = "comment 내용";
         Long commentWriterId = 1L;
         boolean isAnonymous = true;
         Comment comment =
@@ -76,41 +78,45 @@ public class PostRepositoryTest {
         commentRepository.save(comment);
 
         // when
-        List<FindPostResponseSimpleDto> actualFindResponseSimpleDtoList =
-                postRepository.findAllWithComments();
-        FindPostResponseSimpleDto expectedFindResponseSimpleDto1 =
-                FindPostResponseSimpleDto.from(post1, 1L);
-        FindPostResponseSimpleDto expectedFindResponseSimpleDto2 =
-                FindPostResponseSimpleDto.from(post2, 0L);
+        Pageable pageable = PageRequest.of(0, 5);
+        List<Tuple> actualPostList = postRepository.findAllWithComments(pageable);
 
         // then
-        assertEquals(actualFindResponseSimpleDtoList.size(), 2);
+        assertEquals(actualPostList.size(), 2);
 
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(0).getTitle(),
-                expectedFindResponseSimpleDto1.getTitle());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(0).getWriterId(),
-                expectedFindResponseSimpleDto1.getWriterId());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(0).getCreatedDate(),
-                expectedFindResponseSimpleDto1.getCreatedDate());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(0).getCountOfComments(),
-                expectedFindResponseSimpleDto1.getCountOfComments());
+        Post actualPost1 = actualPostList.get(1).get(0, Post.class);
+        Long actualCountOfComments1 = actualPostList.get(1).get(1, Long.class);
+        assertEquals(actualPost1.getTitle(), post1.getTitle());
+        assertEquals(actualPost1.getWriterId(), post1.getWriterId());
+        assertEquals(actualPost1.getCreatedDate(), post1.getCreatedDate());
+        assertEquals(actualCountOfComments1, 1L);
 
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(1).getTitle(),
-                expectedFindResponseSimpleDto2.getTitle());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(1).getWriterId(),
-                expectedFindResponseSimpleDto2.getWriterId());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(1).getCreatedDate(),
-                expectedFindResponseSimpleDto2.getCreatedDate());
-        assertEquals(
-                actualFindResponseSimpleDtoList.get(1).getCountOfComments(),
-                expectedFindResponseSimpleDto2.getCountOfComments());
+        Post actualPost2 = actualPostList.get(0).get(0, Post.class);
+        Long actualCountOfComments2 = actualPostList.get(0).get(1, Long.class);
+        assertEquals(actualPost2.getTitle(), post2.getTitle());
+        assertEquals(actualPost2.getWriterId(), post2.getWriterId());
+        assertEquals(actualPost2.getCreatedDate(), post2.getCreatedDate());
+        assertEquals(actualCountOfComments2, 0L);
+    }
+
+    @Test
+    @DisplayName("전체 post 목록 조회 페이징 처리 성공 테스트")
+    public void post_전체_목록_조회_페이징_처리_성공() throws Exception {
+        // given - post
+        for (int i = 0; i < 10; i++) {
+            String title = "test-title-" + i;
+            String content = "test-content-" + i;
+            Long writerId = 0L;
+            Post post = Post.builder().title(title).content(content).writerId(writerId).build();
+            postRepository.save(post);
+        }
+
+        // when
+        Pageable pageable = PageRequest.of(0, 5);
+        List<Tuple> actualPostList = postRepository.findAllWithComments(pageable);
+
+        // then
+        assertEquals(actualPostList.size(), 5);
     }
 
     @AfterEach
